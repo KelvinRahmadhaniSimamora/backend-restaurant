8163721be9b85e0fe7377292108fc00141d5dd60
kelvin <kelvinrahmadhani4@gmail.com>
2023-01-19 20:18:09 +0700

    first commit

---
+++ src/admin/app.js
@@ -0,0 +1,14 @@
+import AuthLogo from "./extensions/my-logo.png";
+import MenuLogo from "./extensions/logo.png";
+
+export default {
+  config: {
+    auth: {
+      logo: AuthLogo,
+    },
+    menu: {
+      logo: MenuLogo,
+    },
+  },
+  bootstrap(){}
+};
---
+++ src/api/menu-favorit/routes/menu-favorit.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * menu-favorit router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::menu-favorit.menu-favorit');
